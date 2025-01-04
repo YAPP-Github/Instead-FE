f148c67db09595663ee1db1d2502a93316805dac
kangnayeon <seoultownroad@naver.com>
2025-01-04 18:00:35 +0900

    fix: 테스트 컴포넌트 제거

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from '@repo/ui';
-
 export default function Home() {
-  return (
-    <div>
-      웹 1팀 파이팅!<Button>ddd</Button>
-    </div>
-  );
+  return <div>웹 1팀 파이팅!</div>;
 }
