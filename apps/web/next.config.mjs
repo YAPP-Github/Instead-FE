0f194067f98cc2df89abc422083190c30378a719
kangnayeon <seoultownroad@naver.com>
2025-01-04 17:55:57 +0900

    fix: 공통 패키지 루트로 끌어올리기, 패키지 버전 의존성 적용

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,10 +2,6 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    scrollRestoration: true,
-  },
-};
+const nextConfig = {};
 
 export default withVanillaExtract(nextConfig);
