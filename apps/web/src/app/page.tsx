e5ef74c0d9bc0b64a17b89b4f576d566c4074367
kangnayeon <seoultownroad@naver.com>
2025-01-10 00:23:58 +0900

    fix(packages/theme): 컬러 토큰 변경에 따른 dark, theme 토큰 변경

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       웹 1팀 파이팅!
-      <Icon width={100} height={100} name="stack" fill="semantic.blue" />
+      <Icon width={100} height={100} name="stack" fill="blue" />
     </div>
   );
 }
