5723efc4d109cc5f25de4d79daa0030af64d5131
kangnayeon <seoultownroad@naver.com>
2025-01-10 00:32:31 +0900

    chore: 주석 제거

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,6 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
 
-// TODO 디자인 시스템 패키지 추가 필요
-// https://vanilla-extract.style/documentation/integrations/next/
 const nextConfig = {
   transpilePackages: ['@repo/ui', '@repo/theme'],
 };
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,13 +1,2 @@
-<<<<<<< HEAD
 export * from './Spacing/Spacing';
-<<<<<<< HEAD
-=======
 export * from './Icon/Icon';
-<<<<<<< HEAD
->>>>>>> 33b4d38 (feat(packages/ui): Icon 컴포넌트)
-=======
-export * from './Spacing/Spacing';
->>>>>>> 4afce2e ([Feat] Spacing 컴포넌트 추가 (#49))
-=======
-export * from './Icon/Icon';
->>>>>>> a244d57 (fix: resolve conflicts)
