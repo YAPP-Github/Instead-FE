762a8e707d49a4623877c6206c50f2ef3277a090
강나연 <103591752+kongnayeon@users.noreply.github.com>
2025-01-09 23:12:48 +0900

    [Feat] Spacing 컴포넌트 추가 (#49)

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -2,4 +2,8 @@
 export * from './Spacing/Spacing';
 =======
 export * from './Icon/Icon';
+<<<<<<< HEAD
 >>>>>>> 33b4d38 (feat(packages/ui): Icon 컴포넌트)
+=======
+export * from './Spacing/Spacing';
+>>>>>>> 4afce2e ([Feat] Spacing 컴포넌트 추가 (#49))
