f0764adc36ab2fa1200c07ce0a698ff71a5632c3
minseong <kwan0324@naver.com>
2025-01-13 02:34:34 +0900

    fix(apps/web): Providers 컴포넌트 분리, OverlayProvider 이동

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Icon, Toast } from '@repo/ui';
-import { overlay, OverlayProvider } from 'overlay-kit';
+import { overlay } from 'overlay-kit';
 
 export default function Home() {
   const notify1 = () =>
@@ -29,15 +29,13 @@ export default function Home() {
     ));
 
   return (
-    <OverlayProvider>
-      <div>
-        웹 1팀 파이팅!
-        <Icon size={24} name="stack" type="stroke" />
-        <Icon size={24} name="stack" type="fill" />
-        <Icon size={24} name="stack" type="stroke" color="warning300" />
-        <button onClick={notify1}>success 토스트 열기</button>
-        <button onClick={notify2}>warning 토스트 열기</button>
-      </div>
-    </OverlayProvider>
+    <div>
+      웹 1팀 파이팅!
+      <Icon size={24} name="stack" type="stroke" />
+      <Icon size={24} name="stack" type="fill" />
+      <Icon size={24} name="stack" type="stroke" color="warning300" />
+      <button onClick={notify1}>success 토스트 열기</button>
+      <button onClick={notify2}>warning 토스트 열기</button>
+    </div>
   );
 }
---
+++ apps/web/src/components/Providers/Providers.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { ThemeProvider } from '@repo/theme';
+import { OverlayProvider } from 'overlay-kit';
+import { ReactNode } from 'react';
+
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <ThemeProvider theme="light">
+      <OverlayProvider>{children}</OverlayProvider>
+    </ThemeProvider>
+  );
+}
