17dee67e99eca19056fb7e4013ae929ab4cf66d5
강나연 <103591752+kongnayeon@users.noreply.github.com>
2025-01-19 10:41:45 +0900

    [Feat] Spinner 컴포넌트 추가 (#69)
    * feat(packages/ui): Spinner 컴포넌트
    
    * feat(packages/ui): Spinner span 태그로 감싸기
    
    * fix(packages/ui): SpinnerColorType 추가

---
+++ packages/ui/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { lotties } from '../LottieAnimation/assets';
+import {
+  LottieAnimation,
+  LottieAnimationProps,
+} from '../LottieAnimation/LottieAnimation';
+
+type LottieType = keyof typeof lotties;
+
+type SpinnerColorType = 'black' | 'white';
+
+const SpinnerColor: Record<SpinnerColorType, LottieType> = {
+  black: 'loadingBlack',
+  white: 'loadingWhite',
+};
+
+export type SpinnerProps = {
+  color?: keyof typeof SpinnerColor;
+} & Omit<LottieAnimationProps, 'animationData'>;
+
+/**
+ * @param {SpinnerProps} props - 스피너 속성
+ * @property {SpinnerColor} [color='white'] - 스피너 색상 선택
+ * @property {LottieAnimationProps} [otherProps] - LottieAnimation의 props
+ */
+export const Spinner = forwardRef<HTMLSpanElement, SpinnerProps>(
+  ({ color = 'white', width = '4rem', height = '4rem', ...rest }) => {
+    return (
+      <span>
+        <LottieAnimation
+          animationData={SpinnerColor[color]}
+          width={width}
+          height={height}
+          {...rest}
+        />
+      </span>
+    );
+  }
+);
+
+Spinner.displayName = 'Spinner';
