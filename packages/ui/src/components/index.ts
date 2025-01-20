0bfdce771b42df2aeeeeccb6ff2deba4178c3f9a
kangnayeon <seoultownroad@naver.com>
2025-01-20 23:18:48 +0900

    feat(packages/ui) 컴포넌트 별 배럴 파일 생성

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,33 +1,11 @@
-export { IconButton } from './IconButton/IconButton';
-export type { IconButtonProps } from './IconButton/IconButton';
-export { Spacing } from './Spacing/Spacing';
-export type { SpacingDirection, SpacingProps } from './Spacing/Spacing';
-export { icons } from './Icon/assets';
-export { Icon } from './Icon/Icon';
-export type { IconName, IconProps } from './Icon/Icon';
-export { Toast } from './Toast';
-export type { ToastProps, ToastType, ToastIconProps } from './Toast';
-export { Text } from './Text/Text.subComponents';
-export type { AllowedTags, TextProps } from './Text/Text';
-export { Button } from './Button/Button';
-export type { ButtonSize, ButtonVariant, ButtonProps } from './Button/Button';
-export { Badge } from './Badge/Badge';
-export type { BadgeProps } from './Badge/Badge';
-export { Checkbox } from './Checkbox/Checkbox';
-export type { CheckboxProps } from './Checkbox/Checkbox';
-export { Label } from './Label/Label';
-export type { LabelProps, LabelVariant } from './Label/Label';
-export { Breadcrumb } from './Breadcrumb/Breadcrumb';
-export type {
-  BreadcrumbProps,
-  BreadcrumbItemProps,
-  BreadcrumbSeparatorProps,
-} from './Breadcrumb/Breadcrumb';
-export { TextField } from './TextField/TextField';
-export type {
-  TextFieldProps,
-  TextFieldLabelProps,
-  TextFieldInputProps,
-  TextFieldSubmitProps,
-  TextFieldCounterProps,
-} from './TextField/TextField';
+export * from './IconButton';
+export * from './Spacing';
+export * from './Icon';
+export * from './Toast';
+export * from './Text';
+export * from './Button';
+export * from './Badge';
+export * from './Checkbox';
+export * from './Label';
+export * from './Breadcrumb';
+export * from './TextField';
