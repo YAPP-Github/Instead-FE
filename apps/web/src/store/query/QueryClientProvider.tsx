b0fd90abd4bc63c4829148e85a789d4c576d6376
minseong <kwan0324@naver.com>
2025-02-02 06:01:48 +0900

    feat(apps/web): tanstack-query 셋팅

---
+++ apps/web/src/store/query/QueryClientProvider.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { QueryClientProvider as _QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+import { getQueryClient } from './getQueryClient';
+
+export function QueryClientProvider({ children }: { children: ReactNode }) {
+  const queryClient = getQueryClient();
+
+  return (
+    <_QueryClientProvider client={queryClient}>{children}</_QueryClientProvider>
+  );
+}
