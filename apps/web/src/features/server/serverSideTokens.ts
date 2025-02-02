3cd83a19ea42e498e1987d8bad4c02777e6d2ab5
minseong <kwan0324@naver.com>
2025-02-03 04:55:16 +0900

    fix(apps/web): 토큰 string으로 관리

--- apps/web/src/features/server/serverSideTokens.ts
+++ apps/web/src/features/server/serverSideTokens.ts
@@ -5,6 +5,6 @@ import { cookies } from 'next/headers';
  * @description 서버에서 쿠키 가져오기
  */
 export const getServerSideTokens = () => ({
-  accessToken: getCookie('accessToken', { cookies })?.toString() ?? null,
-  refreshToken: getCookie('refreshToken', { cookies })?.toString() ?? null,
+  accessToken: getCookie('accessToken', { cookies })?.toString() ?? '',
+  refreshToken: getCookie('refreshToken', { cookies })?.toString() ?? '',
 });
