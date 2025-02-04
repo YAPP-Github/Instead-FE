bc62e0921600a231b29bb973da73ee6daea11e57
minseong <kwan0324@naver.com>
2025-02-05 03:24:15 +0900

    fix(apps/web): NavBar 레이아웃 수정

--- apps/web/src/components/common/NavBar/NavBar.tsx
+++ apps/web/src/components/common/NavBar/NavBar.tsx
@@ -4,14 +4,22 @@ import { ReactNode } from 'react';
 import * as styles from './NavBar.css';
 
 export type NavBarProps = {
-  leftAddon: ReactNode;
-  rightAddon: ReactNode;
+  leftAddon?: ReactNode;
+  rightAddon?: ReactNode;
   isScrolled: boolean;
 };
 
 export function NavBar({ leftAddon, rightAddon, isScrolled }: NavBarProps) {
+  const getNavBarClassName = () => {
+    const classes = [styles.navBar];
+    if (isScrolled) classes.push(styles.scrolled);
+    if (leftAddon && rightAddon) classes.push(styles.bothAddons);
+    else if (rightAddon && !leftAddon) classes.push(styles.rightAddonOnly);
+    return classes.join(' ');
+  };
+
   return (
-    <div className={`${styles.navBar} ${isScrolled ? styles.scrolled : ''}`}>
+    <div className={getNavBarClassName()}>
       {leftAddon}
       {rightAddon}
     </div>
