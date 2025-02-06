f5b82d31c98c92fcab9f1b422f071b048a6f96fb
minseong <kwan0324@naver.com>
2025-02-07 06:22:57 +0900

    fix(apps/web): invalidate 제거. 데이터 패칭으로 인해 버벅임

--- apps/web/src/store/mutation/useUpdatePostsMutation.ts
+++ apps/web/src/store/mutation/useUpdatePostsMutation.ts
@@ -1,9 +1,8 @@
 import { PUT } from '@web/shared/server';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { useToast } from '@repo/ui/hooks';
 import { EditPageParams } from '@web/app/(prompt)/edit/[agentId]/[postGroupId]/types';
 import { PostStatus } from '@web/types/post';
-import { getAllPostsQueryOptions } from '../query/useGetAllPostsQuery';
 
 interface UpdatePostPayload {
   postId?: number;
@@ -26,17 +25,11 @@ export function useUpdatePostsMutation({
   agentId,
   postGroupId,
 }: EditPageParams) {
-  const queryClient = useQueryClient();
   const toast = useToast();
 
   return useMutation({
     mutationFn: (data: UpdatePostsRequest) =>
       PUT(`agents/${agentId}/post-groups/${postGroupId}/posts`, data),
-    onSuccess: () => {
-      queryClient.invalidateQueries(
-        getAllPostsQueryOptions({ agentId, postGroupId })
-      );
-    },
     onError: (error) => {
       if (error instanceof Error) {
         toast.error(error.message);
