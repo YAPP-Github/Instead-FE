f15331567ef427ccb39c313da93ec410fb8f9b48
minseong <kwan0324@naver.com>
2025-02-14 04:42:42 +0900

    chore(packages/ui): motion props 상수화

--- packages/ui/src/components/GradientAnimatedText/GradientAnimatedText.tsx
+++ packages/ui/src/components/GradientAnimatedText/GradientAnimatedText.tsx
@@ -1,4 +1,4 @@
-import { motion, HTMLMotionProps } from 'motion/react';
+import { motion, HTMLMotionProps, MotionProps } from 'motion/react';
 import * as styles from './GradientAnimatedText.css';
 import { ReactNode, forwardRef } from 'react';
 import { Slot } from '@radix-ui/react-slot';
@@ -7,6 +7,28 @@ export type GradientAnimatedTextProps = {
   children: ReactNode;
   asChild?: boolean;
 } & HTMLMotionProps<'h1'>;
+
+const animationOptions = {
+  initial: {
+    y: '35vh',
+    scale: 2,
+    x: '-50%',
+    left: '50%',
+    position: 'absolute',
+  },
+  animate: {
+    y: 0,
+    scale: 1,
+    x: 0,
+    left: 'auto',
+    position: 'relative',
+  },
+  transition: {
+    type: 'spring',
+    duration: 0.6,
+    bounce: 0.22,
+  },
+} as MotionProps;
 
 /**
  * 그라디언트 애니메이션이 적용된 텍스트 컴포넌트입니다.
@@ -30,25 +52,7 @@ export const GradientAnimatedText = forwardRef<
       <Slot ref={ref}>
         <motion.div
           className={`${styles.gradientTitleStyle} ${className}`}
-          initial={{
-            y: '35vh',
-            scale: 2,
-            x: '-50%',
-            left: '50%',
-            position: 'absolute',
-          }}
-          animate={{
-            y: 0,
-            scale: 1,
-            x: 0,
-            left: 'auto',
-            position: 'relative',
-          }}
-          transition={{
-            type: 'spring',
-            duration: 0.6,
-            bounce: 0.22,
-          }}
+          {...animationOptions}
           {...props}
         >
           {children}
@@ -61,25 +65,7 @@ export const GradientAnimatedText = forwardRef<
     <motion.h1
       ref={ref}
       className={`${styles.gradientTitleStyle} ${className}`}
-      initial={{
-        y: '35vh',
-        scale: 2,
-        x: '-50%',
-        left: '50%',
-        position: 'absolute',
-      }}
-      animate={{
-        y: 0,
-        scale: 1,
-        x: 0,
-        left: 'auto',
-        position: 'relative',
-      }}
-      transition={{
-        type: 'spring',
-        duration: 0.6,
-        bounce: 0.22,
-      }}
+      {...animationOptions}
       {...props}
     >
       {children}
