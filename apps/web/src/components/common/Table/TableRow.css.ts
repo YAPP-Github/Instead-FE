56df1772c04a5ee5dcbef7c1dbbab77327f260a9
minseong <kwan0324@naver.com>
2025-02-20 06:43:42 +0900

    feat(apps/web): Table 컴포넌트

--- apps/web/src/components/common/Table/TableRow.css.ts
+++ apps/web/src/components/common/Table/TableRow.css.ts
@@ -2,17 +2,17 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '@repo/theme';
 
 export const tableRowStyle = style({
-  display: 'flex',
-  alignItems: 'center',
-  width: '100%',
-  cursor: 'grab',
-  borderBottom: `1px solid ${vars.colors.grey200}`,
+  height: '6.4rem',
 });
 
 export const tableRowCellStyle = style({
-  flexShrink: 0,
-  display: 'flex',
-  alignItems: 'center',
+  borderBottom: `0.1rem solid ${vars.colors.grey100}`,
+  padding: `${vars.space[16]} 0`,
+  selectors: {
+    '&:first-child': {
+      paddingLeft: vars.space[16],
+    },
+  },
 });
 
 export const contentItemWrapper = style({
