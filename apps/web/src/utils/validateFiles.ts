6a9544a754756a6aabb804dd204fee786821c407
minseong <kwan0324@naver.com>
2025-02-21 23:05:01 +0900

    fix(apps/web): 5mb로 제한

--- apps/web/src/utils/validateFiles.ts
+++ apps/web/src/utils/validateFiles.ts
@@ -1,10 +1,10 @@
-/**
- * 이미지가 맞는지 검증한다
- */
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+
 export function validateFiles(files: File[]) {
   const isValidFiles = files.every((file) => {
     const isImage = file.type.startsWith('image/');
-    return isImage;
+    const isValidSize = file.size <= MAX_FILE_SIZE;
+    return isImage && isValidSize;
   });
 
   return isValidFiles;
